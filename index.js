8039ca9cdc3a32ca2371c19c9901e1ace2318b56
Christopher Marzulli <chriszulli15@gmail.com>
2022-04-29 15:01:59 -0400

    commit tree f723a3200e208f68a4a27255657e5785e7a2c327

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin(record){
+   const win = record.find(record => record.result === "W")
+    return win ? win.year : undefined
+   //    if (win === "W"){
+//        return record.year
+//    }
+}
